2d297638512178c820c066c1189fc81b953a21d6
iieono <itstianmi@gmail.com>
2023-09-15 20:25:47 +0500

    commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,12 +7,13 @@ export const Cart = ({cartItems, setIsCart, setViewOrder}) => {
     for(const order of cartItems){
       total_price += (order.price * order.count)
     }
-    // window.Telegram.WebApp.sendData({
-    //   cart : cartItems,
-    //   comment : comment,
-    //   total_price : total_price,
-    // })
-    window.Telegram.WebApp.showConfirm(`${total_price} hello`)
+    window.Telegram.WebApp.showConfirm(`Are you sure? Total price: ${total_price.toLocaleString('fr')} so'm`, ()=>{
+      window.Telegram.WebApp.sendData({
+        cart : cartItems,
+        comment : comment,
+        total_price : total_price,
+      })
+    })
   }
   useEffect(()=>{
     let total_price = 0
