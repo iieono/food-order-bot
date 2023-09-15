d3095af785bafe0780562496fe8a448b92a24e5d
iieono <itstianmi@gmail.com>
2023-09-15 15:18:36 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
-import { useState } from 'react';
-import './App.css';
-import { Items } from './pages/Items';
-import { Cart } from './pages/Cart'
+import { useState } from "react";
+import "./App.css";
+import { Items } from "./pages/Items";
+import { Cart } from "./pages/Cart";
 
-const categories = ['111', '2222222222', '333333', '444444444444', '5555']
+const categories = ["111", "2222222222", "333333", "444444444444", "5555"];
 
 function App() {
-  console.log(window.Telegram.WebApp)
+  console.log(window.Telegram.WebApp);
   // window.Telegram.WebApp.MainButton.show()
   // window.Telegram.WebApp.MainButton.setText('View Order')
   // window.Telegram.WebApp.MainButton.onClick(()=>setIsCart(state => !state))
 
-  const [isCart, setIsCart] = useState(false)
-  const [cartItems, setCartItems] = useState([])
-  const [categories, setCategories] = useState(['hello', 'holo hola hola', 'bonkerrs', 'cha cha cha, cha cha'])
-  const [catIndex, setCatIndex] = useState(1)
+  const [isCart, setIsCart] = useState(true);
+  const [cartItems, setCartItems] = useState([]);
+  const [categories, setCategories] = useState([
+    "hello",
+    "holo hola hola",
+    "bonkerrs",
+    "cha cha cha, cha cha",
+  ]);
+  const [catIndex, setCatIndex] = useState(1);
+  
 
-  const handleCatgory = (index)=>{
-    if(index >=0 && index < categories.length){
-      setCatIndex(index)
+  const handleCatgory = (index) => {
+    if (index >= 0 && index < categories.length) {
+      setCatIndex(index);
     }
-  }
+    setIsCart(true)
+  };
 
   return (
     <div className="App">
-      <div className='container'>
-        {
-          !isCart ? (
-          <div className='container-main'>
-            <div className='category-container'>
-                <button className='prev-category category-item' onClick={()=>handleCatgory(catIndex-1)}>{categories[catIndex-1]}</button>
-                <button className='curr-category category-item'>{categories[catIndex]}</button>
-                <button className='next-category category-item' onClick={()=>handleCatgory(catIndex+1)}>{categories[catIndex+1]}</button>
+      <div className="container">
+        {!isCart ? (
+          <div className="container-main">
+            <div className="category-container">
+              <button
+                className="prev-category category-item"
+                onClick={() => handleCatgory(catIndex - 1)}
+              >
+                {categories[catIndex - 1]}
+              </button>
+              <button className="curr-category category-item">
+                {categories[catIndex]}
+              </button>
+              <button
+                className="next-category category-item"
+                onClick={() => handleCatgory(catIndex + 1)}
+              >
+                {categories[catIndex + 1]}
+              </button>
             </div>
             <Items />
           </div>
-
-          ):(
-            <Cart cartItems={cartItems} />
-          )
-        }
-
-      
-    </div>
+        ) : (
+          <Cart cartItems={cartItems} setIsCart={setIsCart} />
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export const Cart = (cartItems) => {
+export const Cart = ({cartItems, setIsCart}) => {
+  
+  const orderList = [
+    {
+      item_name : 'Pizza',
+      price : 65000,
+      count : 3,
+      description: 'Pizza peperonni spicy one' 
+    },
+    {
+      item_name : 'Cola',
+      price : 10000,
+      count : 1, 
+      description: 'Cola cola cola cola cola cola cola caol cola' 
+    }
+  ]
+  useEffect(()=>{
+    let total_price = 0
+    for(order of orderList){
+      full_price += (order.price * order.count)
+    }
+
+    window.Telegram.WebApp.MainButton.show()
+    window.Telegram.WebApp.MainButton.setText(`Pay ${total_price}`)
+    window.Telegram.WebApp.MainButton.onClick(()=>setIsCart(false))
+  },[])
   return (
-    <div>Cart</div>
+      <div className='cart-orders'>
+        <div className='cart-header-container'>
+          <p className='cart-header'>YOUR ORDER</p>
+          <button className='cart-edit-button' onClick={ ()=> setIsCart(false) }>Edit</button>
+        </div>
+        <div className='cart-list-container'>
+            { orderList && orderList.map((order)=>{
+              return(
+                <div className='cart-item-container'>
+                  <div className='cart-image-container'>
+                    <img src='https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992_640.jpg' alt='image' className='cart-item-image'/>
+                  </div>
+                  <div className='order-item-details'>
+                    <div className='order-item-details-header'>
+                      <p>{order.item_name} <b className='bold-count'>{order.count}x</b></p>
+                      <p>{order.count * order.price} sum</p>
+                    </div>
+                    <div className='order-item-details-desc'>
+                      <p className='comment-text'>{order.description}</p>
+                    </div>
+                  </div>
+                </div>
+              )
+            }) }
+        </div>
+        <div className='extra-details'>
+          <input type='text' placeholder='Add comment...' className='comment-input'/>
+          <p className='info-comment'>Any special requests, details, final wishes etc.</p>
+        </div>
+      </div>
   )
 }
