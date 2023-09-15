25b825b0fb2984e28be3a16f8ed34518f30d792f
iieono <itstianmi@gmail.com>
2023-09-15 10:19:47 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+import { Items } from './pages/Items';
+import { Cart } from './pages/Cart'
+
+const categories = ['111', '2222222222', '333333', '444444444444', '5555']
 
 function App() {
   console.log(window.Telegram.WebApp)
+
+  const [isCart, setIsCart] = useState(false)
+
   return (
     <div className="App">
+      <div className='container'>
+        {
+          !isCart ? (
+          <div className='container-main'>
+            <div className='category-container'>
+              <button className='curr-category category-item'>Fast Food</button>
+            </div>
+            <Items />
+          </div>
+          ):(
+            <Cart />
+          )
+        }
+
+      
+    </div>
     </div>
   );
 }
